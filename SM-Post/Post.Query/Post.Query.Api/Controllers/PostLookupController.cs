82e80cc7f06d540ec6affb46571d93825e92dd28
Eugene A <asdnvx14@yahoo.com>
2023-11-10 11:19:37 +0200

    Section 16

---
+++ SM-Post/Post.Query/Post.Query.Api/Controllers/PostLookupController.cs
@@ -0,0 +1,136 @@
+﻿using CQRS.Core.Infrastucture;
+using Microsoft.AspNetCore.Mvc;
+using Post.Common.DTOs;
+using Post.Query.Api.DTOs;
+using Post.Query.Api.Queries;
+using Post.Query.Domain.Entities;
+using System.Security.Cryptography;
+
+namespace Post.Query.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class PostLookupController : ControllerBase
+    {
+        private readonly ILogger<PostLookupController> _logger;
+        private readonly IQueryDispatcher<PostEntity> _queryDispatcher;
+
+        public PostLookupController(ILogger<PostLookupController> logger, IQueryDispatcher<PostEntity> queryDispatcher)
+        {
+            _logger = logger;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllPostAsync()
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindAllPostQuery());
+
+                return NormallResponse(posts);
+
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error processing request to retrieve all posts";
+                return ErorResponse(ex, SAFE_ERROR_MESSAGE);
+            }
+        }
+
+        [HttpGet("byId/{postId}")]
+        public async Task<ActionResult> GetPostByIdAsync(Guid postId)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostByIdQuery { Id = postId });
+
+                if (posts == null || !posts.Any())
+                    return NoContent();
+
+                return Ok(new PostLookupResponse
+                {
+                    Posts = posts,
+                    Message = $"Returned post"
+                });
+
+            }
+            catch(Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error processing request to find the post by id";
+                return ErorResponse(ex, SAFE_ERROR_MESSAGE);
+            }
+        }
+
+        [HttpGet("byAuthor/{author}")]
+        public async Task<ActionResult> GetPostsByAuthorAsync(string author)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostByAuthorQuery { Author  = author });
+
+                return NormallResponse(posts);
+
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error processing request to find the posts by author";
+                return ErorResponse(ex, SAFE_ERROR_MESSAGE);
+            }
+        }
+
+        private ActionResult ErorResponse(Exception ex, string SAFE_ERROR_MESSAGE)
+        {
+            _logger.LogError(ex, SAFE_ERROR_MESSAGE);
+
+            return StatusCode(StatusCodes.Status500InternalServerError, new BaseResponse
+            {
+                Message = SAFE_ERROR_MESSAGE
+            });
+        }
+
+        [HttpGet("withComments")]
+        public async Task<ActionResult> GetPostsWithCommentsAsync()
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostWithCommentsQuery());
+                return NormallResponse(posts);
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error processing request to find the posts with comments";
+                return ErorResponse(ex, SAFE_ERROR_MESSAGE);
+            }
+        }
+
+        [HttpGet("withLikes/{numberOfLikes}")]
+        public async Task<ActionResult> GetPostsWithLikesAsync(int numberOfLikes)
+        {
+            try
+            {
+                var posts = await _queryDispatcher.SendAsync(new FindPostsWithLikesQuery() { NumberOfLikes = numberOfLikes});
+                return NormallResponse(posts);
+            }
+            catch (Exception ex)
+            {
+                const string SAFE_ERROR_MESSAGE = "Error processing request to find the posts with likes";
+                return ErorResponse(ex, SAFE_ERROR_MESSAGE);
+            }
+        }
+
+        private ActionResult NormallResponse(List<PostEntity> posts)
+        {
+            if (posts == null || !posts.Any()) 
+                return NoContent();
+
+            var count = posts.Count;
+
+            return Ok(new PostLookupResponse
+            {
+                Posts = posts,
+                Message = $"Returned {count} post{(count > 1 ? "s" : string.Empty)}"
+            });
+        }
+    }
+}
---
+++ SM-Post/Post.Query/Post.Query.Infrastructure/Dispatchers/QueryDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using CQRS.Core.Infrastucture;
+using CQRS.Core.Queries;
+using Post.Query.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Query.Infrastructure.Dispatchers
+{
+    internal class QueryDispatcher : IQueryDispatcher<PostEntity>
+    {
+        private readonly Dictionary<Type, Func<BaseQuery, Task<List<PostEntity>>>> _handlers = new();
+
+        public void RegisterHandler<TQuery>(Func<TQuery, Task<List<PostEntity>>> handler) where TQuery : BaseQuery
+        {
+            if (_handlers.ContainsKey(typeof(TQuery))) throw new IndexOutOfRangeException("Can't register same type twice");
+
+            _handlers.Add(typeof(TQuery), x=> handler((TQuery)x));
+        }
+
+        public async Task<List<PostEntity>> SendAsync(BaseQuery query)
+        {
+            if (_handlers.TryGetValue(query.GetType(), out Func<BaseQuery, Task<List<PostEntity>>> handler))
+                return await handler(query);
+
+            throw new ArgumentNullException(nameof(handler), "No query handler was registered");
+        }
+    }
+}
